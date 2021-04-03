90284c42d603dc4fda9ce67985cc2f45860771ea
Reesh Miranda <rtmiranda18@gmail.com>
2021-04-03 13:20:55 +0800

    concatinated the container timeblocks

--- script.js
+++ script.js
@@ -4,19 +4,27 @@ $(document).ready(function () {
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 $(".hour").text(moment().format("hA"));
 
+// use a for loop to get the boxes to show colors 
 for(i=9; i < 19; i++){
     var d = moment(); 
     if (i==d.format("H")) {
-        var color = "red";
+        var color = "present";
     }
     else if (i > d.format("H")) {
-        var color = "green";
+        var color = "future";
     }
     else if (i < d.format("H")) {
-        var color = "gray";
-    }
-    //console.log(i + " = " + color); 
+        var color = "past";
+    } 
     var workTime = i + ":00:00";
-    $(".color").append("<span>" + moment(workTime, "HH:mm:ss").format("hA") + " = " + color + "</span>");
+    $(".container").append(
+        '<div class="row"><div class="hour">' + moment(workTime, "HH:mm:ss").format("hA") + '</div><form class="' + color + '"><input class="' + color + '" type="text" id="event" name="event"></form><button class="saveBtn">Save</button></div>'
+        
+    
+        //'<span>' +  + " = " + color + '</span>'
+    
+    );
+
 }
+
 });
