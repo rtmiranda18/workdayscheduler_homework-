066f8e20d32cac8993f6d964b0c8607f0f81bf0a
Reesh Miranda <rtmiranda18@gmail.com>
2021-04-03 14:11:15 +0800

    input time blocks in html

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ $(document).ready(function () {
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 $(".hour").text(moment().format("hA"));
 
-// 
 
+var i = 9;
 let updateTime = function () {
     let currentTime = moment().format('H')
     // use a for loop to get the boxes to show colors 
-    for(i=9; i < 19; i++){
+    for(i=9; i < 19; i++) { 
         var d = currentTime; 
         if (i==d) {
             var color = "present";
@@ -22,11 +22,16 @@ let updateTime = function () {
         } 
         // concatinate container
         var workTime = i + ":00:00";
-        $(".container").append(
-            '<div class="row"><div class="hour">' + moment(workTime, "HH:mm:ss").format("hA") + '</div><form class="' + color + '"><input class="' + color + '" type="text" id="event" name="event"></form><button class="saveBtn">Save</button></div>'
+        //$(".container").append(
+            //'<div class="row"><div class="hour">' + moment(workTime, "HH:mm:ss").format("hA") + '</div><form class="' + color + '"><input class="' + color + '" type="text" id="event" name="event"></form><button class="saveBtn">Save</button></div>'
         );
     }
 }
+setInterval(updateTime, 100);
 
-
+// Store Data
+localStorage.setItem("test", "this");
+console.log(localStorage);
+// Retrieve Data
+//document.getElementById("result").innerHTML = localStorage.getItem("test");
 });
