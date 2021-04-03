c658eb903937b0232328c08f61f00cecc6a6e49c
Reesh Miranda <rtmiranda18@gmail.com>
2021-04-03 16:01:45 +0800

    prolonged the setInterval time

--- script.js
+++ script.js
@@ -22,33 +22,20 @@ let updateTime = function () {
         } 
         var workTime = i + ":00:00";
         $(".row-" + i + " :input, form.row-" + i).addClass(color);
-        //$("form.row-9").addClass(color);
         // moment(workTime, "HH:mm:ss").format("hA")
         var storedEvent = localStorage.getItem("row-" + i);
         console.log(storedEvent);
         $(".row-" + i + " :input").val(storedEvent);
     }
 }
-
+updateTime();
 $(".saveBtn").click(function() {
     var parentRow = $(this).parent().attr('id');
     var eventValue = $("." + parentRow + " :input").val()
     localStorage.setItem(parentRow, eventValue);
+    updateTime();
   });
   
+setInterval(updateTime, 20000);
 
-function saveEvent(rowhour) {
-    var event = $("." + rowhour + " > :input").val();
-
-    localStorage.setItem("test", event);
-    console.log(localStorage);
-
-}
-setInterval(updateTime, 1000);
-
-// Store Data
-//localStorage.setItem("test", "this");
-//console.log(localStorage);
-// Retrieve Data
-//document.getElementById("result").innerHTML = localStorage.getItem("test");
 });
