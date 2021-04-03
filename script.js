ea54f435274b70fed4fc5cfe9e9f91d2f024e365
Reesh Miranda <rtmiranda18@gmail.com>
2021-04-03 13:32:00 +0800

    reorganized for loop

--- script.js
+++ script.js
@@ -4,27 +4,29 @@ $(document).ready(function () {
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 $(".hour").text(moment().format("hA"));
 
-// use a for loop to get the boxes to show colors 
-for(i=9; i < 19; i++){
-    var d = moment(); 
-    if (i==d.format("H")) {
-        var color = "present";
-    }
-    else if (i > d.format("H")) {
-        var color = "future";
-    }
-    else if (i < d.format("H")) {
-        var color = "past";
-    } 
-    var workTime = i + ":00:00";
-    $(".container").append(
-        '<div class="row"><div class="hour">' + moment(workTime, "HH:mm:ss").format("hA") + '</div><form class="' + color + '"><input class="' + color + '" type="text" id="event" name="event"></form><button class="saveBtn">Save</button></div>'
-        
-    
-        //'<span>' +  + " = " + color + '</span>'
-    
-    );
+// 
 
+let updateTime = function () {
+    let currentTime = moment().format('H')
+    // use a for loop to get the boxes to show colors 
+    for(i=9; i < 19; i++){
+        var d = currentTime; 
+        if (i==d) {
+            var color = "present";
+        }
+        else if (i > d) {
+            var color = "future";
+        }
+        else if (i < d) {
+            var color = "past";
+        } 
+        // concatinate container
+        var workTime = i + ":00:00";
+        $(".container").append(
+            '<div class="row"><div class="hour">' + moment(workTime, "HH:mm:ss").format("hA") + '</div><form class="' + color + '"><input class="' + color + '" type="text" id="event" name="event"></form><button class="saveBtn">Save</button></div>'
+        );
+    }
 }
+
 
 });
